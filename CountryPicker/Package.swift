5e7fb1ea28e838ad9df73a4b6d175b9f51ec29c8
Ilyar Mnazhdin <ilyarmnzhdn@gmail.com>
2019-10-02 22:48:37 +0600

    fix package swift

--- CountryPicker/Package.swift
+++ CountryPicker/Package.swift
@@ -5,24 +5,22 @@ import PackageDescription
 
 let package = Package(
     name: "CountryPicker",
+    platforms: [
+        .iOS(.v11)
+    ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "CountryPicker",
             targets: ["CountryPicker"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "CountryPicker",
-            dependencies: []),
+            name: "CountryPicker"),
         .testTarget(
             name: "CountryPickerTests",
             dependencies: ["CountryPicker"]),
+    ],
+    swiftLanguageVersions: [
+        .v5
     ]
 )
